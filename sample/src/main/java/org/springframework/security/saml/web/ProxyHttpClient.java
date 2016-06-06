792fd5a7b394446c60609539e071e4a1fc076003
Hidemoto Yamauchi <Hidemoto.Yamauchi@capitalone.com>
2016-06-06 08:29:48 -0500

    Rename HttpClient Subclass.

--- sample/src/main/java/org/springframework/security/saml/web/ProxyHttpClient.java
+++ sample/src/main/java/org/springframework/security/saml/web/ProxyHttpClient.java
@@ -4,17 +4,16 @@ import org.apache.commons.httpclient.*;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.saml.metadata.CachingMetadataManager;
 
 /**
  * @author Hidemoto Yamauchi
  */
-public class C1ProxyClient extends HttpClient {
-    private final Logger log = LoggerFactory.getLogger(C1ProxyClient.class);
+public class ProxyHttpClient extends HttpClient {
+    private final Logger log = LoggerFactory.getLogger(ProxyHttpClient.class);
 
-    public C1ProxyClient() {
+    public ProxyHttpClient() {
         super();
-        log.info("Entering C1ProxyClient Constructor");
+        log.info("Entering ProxyHttpClient Constructor");
         this.getHostConfiguration().setProxy("proxy.kdc.capitalone.com", 8099);
         this.getState().setProxyCredentials(AuthScope.ANY, new UsernamePasswordCredentials("arj866", "Natuki76"));
         this.getParams().setAuthenticationPreemptive(true);
